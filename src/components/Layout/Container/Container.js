9f3a7b22ecb10ccda50ed139e3acfaed01148977
Ahmad Saddique <46298081+AhmedSaddique@users.noreply.github.com>
2024-05-25 17:00:40 +0500

    tredella

--- src/components/Layout/Container/Container.js
+++ src/components/Layout/Container/Container.js
@@ -4,7 +4,7 @@ import React, { ReactNode } from 'react';
 
 const Container = ({ children, className }) => {
   return (
-    <div className={`max-w-screen-2xl mx-auto px-2 sm:px-4 md:px-6 ${className}`}>
+    <div className={`max-w-screen-xl mx-auto px-2 sm:px-4 md:px-6 ${className}`}>
       {children}
     </div>
   );
