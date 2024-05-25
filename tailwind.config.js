46514e6813c6b77dd30804ae7310fdefb5fa9cc1
Ahmad Saddique <46298081+AhmedSaddique@users.noreply.github.com>
2024-05-25 17:58:17 +0500

    tredella

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,7 +21,7 @@ module.exports = {
         },
       },
       colors:{
-        primary: "#8F0011 ",
+        primary: "#b30015 ",
         primary2: "#ff1a34",
         secondary:"#FFFCF5",
         dark:"#3A393C",
